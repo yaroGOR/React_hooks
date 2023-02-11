642336a65175dad01506cb79f43f1a091902afcc
Yarik <36842297+yaroGOR@users.noreply.github.com>
2023-02-11 19:41:08 +0200

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
-import React, { useState } from 'react'
-import TodoList from './TodoList'
+import React, { useState, useEffect } from "react";
+import TodoList from "./TodoList";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [toDoTitle, setTodoTitle] = useState("");
   
-  const [todos, setTodos] = useState(
-    [
-    {id: 1, title: 'First todo', completed: false},
-    {id: 2, title: 'Second todo', completed: true},
-  ])
-  const [toDoTitle, setTodoTitle] = useState("")
+  useEffect(() => {
+    const raw = localStorage.getItem("todos") || [];
+    console.log(raw)
+    setTodos(JSON.parse(raw));
+    
+  }, []);
 
-  const addToDo = event => {
-    if(event.key ==='Enter') {
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const addToDo = (event) => {
+    if (event.key === "Enter") {
       setTodos([
         ...todos,
         {
           id: Date.now(),
           title: toDoTitle,
-          completed:false
-        }
-
-      ])
-      setTodoTitle("")
+          completed: false,
+        },
+      ]);
+      setTodoTitle("");
     }
-  }
-    return (
-      <div className="container">
-        <h1>Todo app</h1>
+  };
 
-          <div className="input-field">
-            <input type="text" 
-              value={toDoTitle}
-              onChange={event=>setTodoTitle(event.target.value)}
-              onKeyPress={addToDo}
-            />
-            <label>Todo name</label>
-          </div>
+  return (
+    <div className="container">
+      <h1>Todo app</h1>
 
-          <TodoList todos={todos} />
+      <div className="input-field">
+        <input
+          type="text"
+          value={toDoTitle}
+          onChange={(event) => setTodoTitle(event.target.value)}
+          onKeyPress={addToDo}
+        />
+        <label>Todo name</label>
       </div>
-    );
-  }
+
+      <TodoList todos={todos} />
+    </div>
+  );
+}
